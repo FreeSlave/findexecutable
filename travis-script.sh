389c509506c94dc0843289382599fd8a1cefc30f
Roman Chistokhodov <freeslave93@gmail.com>
2020-07-24 13:34:13 +0300

    Update travis script

--- travis-script.sh
+++ travis-script.sh
@@ -6,7 +6,6 @@ if [ "$USE_DOVERALLS" = "true" ]; then
     wget -O doveralls "https://github.com/ColdenCullen/doveralls/releases/download/v1.3.2/doveralls_linux_travis"
     chmod +x doveralls
     dub test -b unittest-cov --compiler=${DC}
-    rm ..-*
     ./doveralls
 else
     dub test --compiler=${DC}
